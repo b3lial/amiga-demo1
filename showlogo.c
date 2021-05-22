f8f878f301d282d6e32062885be4c893983e21ad
Dr. Christian Ammann <christian.ammann@phobosys.de>
2021-05-22 15:29:39 +0200

    show logo fade in work in progress

--- showlogo.c
+++ showlogo.c
@@ -2,8 +2,11 @@
 #include "starlight/starlight.h"
 
 #include <exec/types.h>
+#include <dos/dos.h>
 
 WORD payloadShowLogoState = SHOWLOGO_INIT;
+UWORD colortable0[SHOWLOGO_BLOB_COLORS];
+struct BitMap* showLogoScreen;
 
 WORD fsmShowLogo(void){    
     if (mouseClick())
@@ -13,14 +16,44 @@ WORD fsmShowLogo(void){
 
     switch(payloadShowLogoState){
         case SHOWLOGO_INIT:
+            initShowLogo();
             payloadShowLogoState = SHOWLOGO_STATIC;
             break;
         case SHOWLOGO_STATIC:
             WaitTOF();
             break;
         case SHOWLOGO_SHUTDOWN:
+            exitShowLogo();
             return MODULE_FINISHED;
     }
 
     return MODULE_CONTINUE;
+}
+
+void initShowLogo(void){
+    UBYTE i = 0;
+    writeLog("\n== initShowLogo() ==\n");
+    memset(colortable0, 0xff, sizeof(colortable0));
+
+    writeLog("\nLoad showlogo screen background bitmap\n");
+    showLogoScreen = createBitMap(SHOWLOGO_BLOB_DEPTH,
+                                      SHOWLOGO_BLOB_WIDTH,
+                                      SHOWLOGO_BLOB_HEIGHT);
+    if(!showLogoScreen){
+        writeLog("Error: Could not allocate memory for showlogo screen bitmap\n");
+        exitShowLogo();
+        exitSystem(RETURN_ERROR);
+    }
+    for (i = 0; i < SHOWLOGO_BLOB_DEPTH; i++)
+    {
+        BltClear(showLogoScreen->Planes[i],
+                 (showLogoScreen->BytesPerRow) * (showLogoScreen->Rows),
+                 1);
+    }
+}
+
+void exitShowLogo(void){
+    if(showLogoScreen){
+        cleanBitMap(showLogoScreen);
+    }
 }
--- showlogo.h
+++ showlogo.h
@@ -7,6 +7,13 @@
 #define SHOWLOGO_STATIC 1
 #define SHOWLOGO_SHUTDOWN 2
 
+#define SHOWLOGO_BLOB_DEPTH 4
+#define SHOWLOGO_BLOB_COLORS 16
+#define SHOWLOGO_BLOB_WIDTH 320
+#define SHOWLOGO_BLOB_HEIGHT 256
+
 WORD fsmShowLogo(void);
+void initShowLogo(void);
+void exitShowLogo(void);
 
 #endif
