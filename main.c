14c3eb115b043cda0ab1e95f7a4051a2487204d9
Dr. Christian Ammann <christian.ammann@phobosys.de>
2021-05-30 18:10:38 +0200

    switched to starlight 1.2.1

--- main.c
+++ main.c
@@ -16,7 +16,7 @@ int main(void) {
 
         switch (fsmCurrentState) {
             case FSM_START:
-                initStarlight(TRUE);
+                initStarlight();
                 fsmNextState = FSM_TEXTSCROLLER;
                 break;
 
