feb9e2dc5651eea9994bdf6d58ff2241fbcc6dd1
Dr. Christian Ammann <christian.ammann@phobosys.de>
2022-03-26 21:47:42 +0100

    added docker run script for build process

---
+++ docker-run.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+SERVICE_NAME=amiga-gcc-build-process
+IMAGE=amiga-gcc-builder
+PROCESS_RUNNING=`docker ps -a --format '{{.Names}}' | grep $SERVICE_NAME`
+
+echo "starting $IMAGE"
+  
+if [ -n "$PROCESS_RUNNING" ]
+then
+    echo terminating old build process
+    docker rm $SERVICE_NAME > /dev/null
+fi
+
+docker run --name=$SERVICE_NAME \
+    -v  /Users/belial/Git/amiga-demo1:/build \
+    --env BUILD_RESULT=demo-1-gcc \
+    $IMAGE:latest
