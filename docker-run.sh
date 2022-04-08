7291e8c0d6dbe2622a425f5ca52ef020e361d231
Dr. Christian Ammann <christian.ammann@phobosys.de>
2022-04-09 00:11:33 +0200

    check whether binary exists and set proper permissions

--- docker-run.sh
+++ docker-run.sh
@@ -3,7 +3,12 @@ SERVICE_NAME=amiga-gcc-build-process
 IMAGE=amiga-gcc-builder
 PROCESS_RUNNING=`docker ps -a --format '{{.Names}}' | grep $SERVICE_NAME`
 
-echo "starting $IMAGE"
+if [ ! -f demo-1-gcc ]
+then
+    echo creating binary file to avoid wrong permissions
+    touch demo-1-gcc
+    chmod +x demo-1-gcc
+fi
   
 if [ -n "$PROCESS_RUNNING" ]
 then
@@ -11,6 +16,8 @@ then
     docker rm $SERVICE_NAME > /dev/null
 fi
 
+echo "starting $IMAGE"
+echo
 docker run --name=$SERVICE_NAME \
     -v  $PWD:/build \
     --env BUILD_RESULT=demo-1-gcc \
