10804448dade2ddb2ec7ff38c713b99063c78e4e
Dr. Christian Ammann <christian.ammann@phobosys.de>
2022-04-09 00:49:21 +0200

    calling planar2chuny

--- chunkyconverter/chunkytests.c
+++ chunkyconverter/chunkytests.c
@@ -1,6 +1,30 @@
 #include "demo1.h"
 
+#define TEST_BITMAP_X 20
+#define TEST_BITMAP_Y 20
+
 void chunkyTests(void) {
+    struct BitMap *testBitmap;
+    struct p2cStruct p2c;
+    UBYTE *chunkyBuffer;
+
+    // just call asm test functions
     printf("testFunc() returned: %d\n", testFunc());
     printf("addFunc(1,2) returned: %d\n", addFunc(1, 2));
+
+    // invoke
+    testBitmap = AllocBitMap(TEST_BITMAP_X, TEST_BITMAP_Y, 8,
+                             BMF_DISPLAYABLE | BMF_CLEAR, NULL);
+    chunkyBuffer = AllocVec(TEST_BITMAP_X * TEST_BITMAP_Y, NULL);
+    p2c.bmap = testBitmap;
+    p2c.startX = 0;
+    p2c.startY = 0;
+    p2c.width = TEST_BITMAP_X;
+    p2c.height = TEST_BITMAP_Y;
+    p2c.chunkybuffer = chunkyBuffer;
+
+    PlanarToChunkyAsm(&p2c);
+
+    FreeVec(chunkyBuffer);
+    FreeBitMap(testBitmap);
 }
