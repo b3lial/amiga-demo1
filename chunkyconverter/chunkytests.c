2a0a1b11d2d5c61fdfbd1f4303c2bb602bfe0cab
Dr. Christian Ammann <christian.ammann@phobosys.de>
2022-04-10 22:36:55 +0200

    create rastport

--- chunkyconverter/chunkytests.c
+++ chunkyconverter/chunkytests.c
@@ -5,7 +5,8 @@
 
 void chunkyTests(void) {
     struct BitMap *testBitmap;
-    struct p2cStruct p2c;
+    struct p2cStruct p2c = {0};
+    struct RastPort rastPort = {0};
     UBYTE *chunkyBuffer;
     UWORD i, j;
 
@@ -13,19 +14,26 @@ void chunkyTests(void) {
     printf("testFunc() returned: %d\n", testFunc());
     printf("addFunc(1,2) returned: %d\n", addFunc(1, 2));
 
-    // invoke
+    // alloc memory for chunky and planar buffers
     testBitmap = AllocBitMap(TEST_BITMAP_X, TEST_BITMAP_Y, 8,
                              BMF_DISPLAYABLE | BMF_CLEAR, NULL);
     chunkyBuffer = AllocVec(TEST_BITMAP_X * TEST_BITMAP_Y, NULL);
+
+    // paint in the planar buffer
+    InitRastPort(&rastPort);
+    rastPort.BitMap = testBitmap;
+    testBitmap->Planes[0][0] = 0xff;
+    
+    // convert planar buffer to chunky
     p2c.bmap = testBitmap;
     p2c.startX = 0;
     p2c.startY = 0;
     p2c.width = TEST_BITMAP_X;
     p2c.height = TEST_BITMAP_Y;
     p2c.chunkybuffer = chunkyBuffer;
-
-    testBitmap->Planes[0][0] = 0xff;
     PlanarToChunkyAsm(&p2c);
+
+    // print content of chunky buffer on screen
     printf("chunky buffer:\n");
     for (i = 0; i < TEST_BITMAP_X; i++) {
         printf("%02d: ", i);
@@ -35,6 +43,7 @@ void chunkyTests(void) {
         printf("\n");
     }
 
+    // free memory and quit
     FreeVec(chunkyBuffer);
     FreeBitMap(testBitmap);
 }
