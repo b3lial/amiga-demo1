1198aee20b1c57bbe8185f7980aa86f47d2a4513
Dr. Christian Ammann <christian.ammann@phobosys.de>
2022-04-15 00:47:13 +0200

    convert chunky back to planar

--- chunkyconverter/chunkytests.c
+++ chunkyconverter/chunkytests.c
@@ -4,31 +4,46 @@
 #define TEST_BITMAP_Y 20
 
 void chunkyTests(void) {
-    struct BitMap *testBitmap;
+    struct BitMap *sourceBitmap;
+    struct BitMap *destinationBitmap;
+
     struct p2cStruct p2c = {0};
+    struct c2pStruct c2p = {0};
+
     struct RastPort rastPort = {0};
     UBYTE *chunkyBuffer;
     UWORD i, j;
 
     // alloc memory for chunky and planar buffers
-    testBitmap = AllocBitMap(TEST_BITMAP_X, TEST_BITMAP_Y, 8,
+    sourceBitmap = AllocBitMap(TEST_BITMAP_X, TEST_BITMAP_Y, 8,
+                             BMF_DISPLAYABLE | BMF_CLEAR, NULL);
+    destinationBitmap = AllocBitMap(TEST_BITMAP_X, TEST_BITMAP_Y, 8,
                              BMF_DISPLAYABLE | BMF_CLEAR, NULL);
     chunkyBuffer = AllocVec(TEST_BITMAP_X * TEST_BITMAP_Y, NULL);
 
     // paint in the planar buffer
     InitRastPort(&rastPort);
-    rastPort.BitMap = testBitmap;
+    rastPort.BitMap = sourceBitmap;
     SetAPen(&rastPort, 6);
     RectFill(&rastPort, 7, 7, 13, 13);
 
     // convert planar buffer to chunky
-    p2c.bmap = testBitmap;
+    p2c.bmap = sourceBitmap;
     p2c.startX = 0;
     p2c.startY = 0;
     p2c.width = TEST_BITMAP_X;
     p2c.height = TEST_BITMAP_Y;
     p2c.chunkybuffer = chunkyBuffer;
     PlanarToChunkyAsm(&p2c);
+
+    // convert chunky back to planar
+    c2p.bmap = destinationBitmap;
+    c2p.startX = 0;
+    c2p.startY = 0;
+    c2p.width = TEST_BITMAP_X;
+    c2p.height = TEST_BITMAP_Y;
+    c2p.chunkybuffer = chunkyBuffer;
+    ChunkyToPlanarAsm(&c2p);
 
     // print content of chunky buffer on screen
     printf("chunky buffer:\n");
@@ -42,5 +57,6 @@ void chunkyTests(void) {
 
     // free memory and quit
     FreeVec(chunkyBuffer);
-    FreeBitMap(testBitmap);
+    FreeBitMap(sourceBitmap);
+    FreeBitMap(destinationBitmap);
 }
