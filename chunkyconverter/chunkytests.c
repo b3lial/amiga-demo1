70e80c404e3afa1303584a615a303db546733537
Dr. Christian Ammann <christian.ammann@phobosys.de>
2022-04-15 16:07:55 +0200

    completed test of chunky converter functions

--- chunkyconverter/chunkytests.c
+++ chunkyconverter/chunkytests.c
@@ -3,7 +3,8 @@
 #define TEST_BITMAP_X 20
 #define TEST_BITMAP_Y 20
 
-void chunkyTests(void) {
+void chunkyTests(void)
+{
     struct BitMap *sourceBitmap;
     struct BitMap *destinationBitmap;
 
@@ -16,9 +17,9 @@ void chunkyTests(void) {
 
     // alloc memory for chunky and planar buffers
     sourceBitmap = AllocBitMap(TEST_BITMAP_X, TEST_BITMAP_Y, 8,
-                             BMF_DISPLAYABLE | BMF_CLEAR, NULL);
+                               BMF_DISPLAYABLE | BMF_CLEAR, NULL);
     destinationBitmap = AllocBitMap(TEST_BITMAP_X, TEST_BITMAP_Y, 8,
-                             BMF_DISPLAYABLE | BMF_CLEAR, NULL);
+                                    BMF_DISPLAYABLE | BMF_CLEAR, NULL);
     chunkyBuffer = AllocVec(TEST_BITMAP_X * TEST_BITMAP_Y, NULL);
 
     // paint in the planar buffer
@@ -45,11 +46,24 @@ void chunkyTests(void) {
     c2p.chunkybuffer = chunkyBuffer;
     ChunkyToPlanarAsm(&c2p);
 
+    // clean chunky before copying back
+    memset(chunkyBuffer, 0, TEST_BITMAP_X * TEST_BITMAP_Y);
+    // convert planar buffer to chunky
+    p2c.bmap = destinationBitmap;
+    p2c.startX = 0;
+    p2c.startY = 0;
+    p2c.width = TEST_BITMAP_X;
+    p2c.height = TEST_BITMAP_Y;
+    p2c.chunkybuffer = chunkyBuffer;
+    PlanarToChunkyAsm(&p2c);
+
     // print content of chunky buffer on screen
     printf("chunky buffer:\n");
-    for (i = 0; i < TEST_BITMAP_X; i++) {
+    for (i = 0; i < TEST_BITMAP_X; i++)
+    {
         printf("%02d: ", i);
-        for (j = 0; j < TEST_BITMAP_Y; j++) {
+        for (j = 0; j < TEST_BITMAP_Y; j++)
+        {
             printf("%x ", chunkyBuffer[i * TEST_BITMAP_X + j]);
         }
         printf("\n");
